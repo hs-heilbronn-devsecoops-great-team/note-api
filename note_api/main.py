1a285d417e12d567c4efd5c1aacbce78fc0637b8
obaid khan <66803234+Obaidkhan19@users.noreply.github.com>
2024-10-12 22:54:29 +0500

    Update main.py

--- note_api/main.py
+++ note_api/main.py
@@ -9,6 +9,19 @@ from starlette.responses import RedirectResponse
 from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
 from .model import Note, CreateNoteRequest
 
+import os
+# use it to instrument, generate, collect, and export telemetry data (metrics, logs, and traces) to help you analyze your software's performance and behavior.
+from opentelemetry import trace
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+
+
 app = FastAPI()
 
 my_backend: Optional[Backend] = None
@@ -36,10 +49,12 @@ def redirect_to_notes() -> None:
 @app.get('/notes')
 def get_notes(backend: Annotated[Backend, Depends(get_backend)]) -> List[Note]:
     keys = backend.keys()
-
     Notes = []
-    for key in keys:
-        Notes.append(backend.get(key))
+    # span is function that gives returns a tuple containing starting and ending index of the matched string
+    with tracer.start_as_current_span("collect_notes") as current_span:
+        for key in keys:
+            Notes.append(backend.get(key))
+        current_span.set_attribute("notes", int(len(Notes)))
     return Notes
 
 
@@ -62,3 +77,13 @@ def create_Note(request: CreateNoteRequest,
     note_id = str(uuid4())
     backend.set(note_id, request)
     return note_id
+
+traceProvider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+traceProvider.add_span_processor(processor)
+if os.environ.get('TRACER_RUNTIME') == 'cloud':
+    cloudProcessor = BatchSpanProcessor(CloudTraceSpanExporter())
+    traceProvider.add_span_processor(cloudProcessor)
+trace.set_tracer_provider(traceProvider)
+tracer = trace.get_tracer("notes.tracer.rrjw")
+FastAPIInstrumentor.instrument_app(app)
